316ba4e4d3a28ebdd0833f25fae51d8e84ef73c6
Da.T <38336173+dariosyo@users.noreply.github.com>
2018-09-10 22:53:01 -0700

    All files added

---
+++ app/data/friends.js
@@ -0,0 +1,40 @@
+var friends = [
+    {
+       "name":"Incredible Hulk",
+       "photo":"https://vignette.wikia.nocookie.net/marvelmovies/images/d/d9/0610a_hulk.jpg/revision/latest?cb=20110909023524",
+       "scores":[5,1,4,4,5,1,2,5,4,1]
+    },
+    {
+       "name":"Iron man",
+       "photo":"https://vignette.wikia.nocookie.net/marveldatabase/images/0/06/Iron_Man_Armor_Model_37.jpg/revision/latest?cb=20170122060858",
+       "scores":[4,2,5,1,3,2,2,1,3,2]
+    },
+    {
+       "name":"Skylar Rose",
+       "photo":"http://i2.wp.com/northstarz.ca/wp-content/uploads/2013/12/Rose.jpg",
+       "scores":[5,2,2,2,4,1,3,2,5,5]
+    },
+    {
+       "name":"Wonder Woman",
+       "photo":"https://feminismandreligion.files.wordpress.com/2017/04/wonder-woman.jpg?w=338&h=450",
+       "scores":[3,3,4,2,2,1,3,2,2,3]
+    },
+     {
+        "name":"Spiderman",
+        "photo":"https://vignette.wikia.nocookie.net/p__/images/b/bb/Spider_man.jpg/revision/latest?cb=20150726031632&path-prefix=protagonist",
+        "scores":[4,3,4,1,5,2,5,3,1,4]
+     },
+       {
+       "name":"Black Panter",
+       "photo":"https://i.pinimg.com/236x/fc/55/e6/fc55e6359145c4488adb4df3bc7c7d1b--martian-manhunter-black-rubber.jpg",
+       "scores":[3,3,4,1,4,3,5,3,5,4]
+    },
+     {
+        "name":"Superman",
+        "photo":"https://i.pinimg.com/originals/2b/54/97/2b549768dc5dba0ee74394b195190918.jpg",
+        "scores":[4,4,2,3,2,2,3,2,4,5]
+     }
+ 
+ ]
+ 
+ module.exports = friends;
